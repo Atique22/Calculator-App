769e8fac89191216ac33fbb04815aa21713b520b
Atique Ahmad <atiqueahmad@Atiques-iMac.local>
2023-01-17 16:04:35 +0500

    react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculate from './components/Calculation';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+       <Calculate/>
+    </>
+       
+  );
+}
+
+export default App;
---
+++ src/components/Calculation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Calculate (){
+
+    return(
+
+        <div>
+                here is my Calculation
+        </div>
+    )
+
+}
