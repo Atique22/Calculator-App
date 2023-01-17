60b84379a4a43cea34575760a2b0ec5af75f1b1b
Atique Ahmad <atiqueahmad@Atiques-iMac.local>
2023-01-17 21:12:17 +0500

    here update

--- src/components/CalBody.js
+++ src/components/CalBody.js
@@ -6,8 +6,7 @@ export default function CalculatorBody ({setExpressionCheck}){
 
     const onClickAction = (e) =>{
         console.log("onClickAction calling ");
-        setExpressionCheck(e);
-       
+        setExpressionCheck(e);  
     }
 
     const buttonArray = ['C','-','+','/','*',0,1,2,3,4,5,6,7,8,9,'='];
