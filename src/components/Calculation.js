da78fdc4b07e034f40f0c4c62dacdd59af8fc633
Atique Ahmad <atiqueahmad@Atiques-iMac.local>
2023-01-19 16:43:21 +0500

    commit calculator

--- src/components/Calculation.js
+++ src/components/Calculation.js
@@ -1,59 +1,97 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import CalculatorBody from './CalBody';
 import CalculatorResult from './CalResult';
 
-export default function Calculate (){
+export default function Calculate() {
 
     const [store_value, setStore_value] = useState('');
     let [expression, setExpression] = useState('');
-    const rapper=(value)=>{
+    const rapper = (value) => {
 
         try {
 
-            if(value.includes("X")){ //backspace
-                
-                expression = expression.substring(0, expression.length-1);
+            if (value.includes("X")) { //backspace
+
+                expression = expression.substring(0, expression.length - 1);
                 setExpression(expression);
-                console.log("backspace is press = "+expression);
+                console.log("backspace is press = " + expression);
             }
-            else if(value.includes("=")){ //for equal operator
+            else if (value.includes("=")) { //for equal operator
                 console.log("equal operator  call");
                 // console.log(eval(expression));
                 setStore_value(expression);
-                setExpression(window.eval(expression));
-    
-            }else if(value.includes("C")){ //for clear..
+
+
+                // const operators = ["+", "-", "/", "%", "*"];
+                // const tokens = expression.split(/\b/);
+                // const stack = [];
+
+                // Split input into an array of numbers and operators
+                var elements = expression.split(/\+|\-|\*|\%|\//);
+                var numbers = elements.map(Number);
+                var operators = expression.replace(/[\d\.]/g, '').split('');
+                var result = numbers.shift();
+
+                while (numbers.length > 0) {
+                    var operator = operators.shift();
+                    var number = numbers.shift();
+            
+                    switch (operator) {
+                        case '+':
+                            result = result + number;
+                            break;
+                        case '-':
+                            result = result - number;
+                            break;
+                        case '*':
+                            result = result * number;
+                            break;
+                        case '/':
+                            result = result / number;
+                            break;
+                        case '%':
+                            result = result / number;
+                            break;
+                        default:
+                            return 'Invalid operator';
+                    }
+                }
+                    setExpression(result);
+                
+            } else if (value.includes("C")) { //for clear..
                 console.log("clear call");
                 // console.log(eval(expression));
                 setExpression('');
-    
-            }else{
-    
-                setExpression((oldValue)=>{
+                setStore_value('');
+
+            } else {
+
+                setExpression((oldValue) => {
+
                     return `${oldValue}${value}`;
                 })
             }
         } catch (error) {
-            alert("Please Enter a valid user input, error -> "+error);
+            alert("Please Enter a valid user input, error -> " + error);
         }
-        
-        
-    }
-    useEffect(()=>{
-        console.log("expression call here value is = "+expression);
-        
-        
-    },[expression])
 
-    return(
-        
-          <Card style={{ width: '25rem', margin: '10rem'}}>
-          <Card.Header>Calculator operators [+,-,/,x,%...]</Card.Header>
+
+    }
+    useEffect(() => {
+        console.log("expression call here value is = " + expression);
+
+
+    }, [expression])
+
+    return (
+
+        <Card style={{ width: '25rem', margin: '10rem' }}>
+            <Card.Header>Calculator operators [+,-,/,x,%...]</Card.Header>
             <CalculatorResult result={expression} oldValue={store_value} />
-            <CalculatorBody setExpressionCheck = {rapper}/>
-          </Card>
-        
+            <CalculatorBody setExpressionCheck={rapper} />
+        </Card>
+
     )
 
 }
